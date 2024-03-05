5a46d808449432362ce4c7b704dd931723487627
Chris Idema <cidema@hedon.nl>
2024-03-05 16:17:36 +0100

    updated readme, cleaned up code

--- floyd-steinberg.py
+++ floyd-steinberg.py
@@ -2,44 +2,6 @@
 import numpy as np
 from PIL import Image
 
-# GREYSCALE = False
-
-# img_name = '1665_Girl_with_a_Pearl_Earring.jpg'
-# img_small_name = '1665_girl_with_a_pearl_earring_sm.jpg'
-# IMG_HEIGHT = 473
-# IMG_WIDTH = 400
-
-
-# try:
-#     img = Image.open(img_small_name)
-#     if GREYSCALE:
-#         img = img.convert('L')
-# except:
-#     img = Image.open(img_name)
-#     if GREYSCALE:
-#         img = img.convert('L')
-
-#     width, height = img.size
-#     new_width = IMG_WIDTH
-#     new_height = int(height * new_width / width)
-#     img = img.resize((new_width, new_height))
-#     #img = img.resize((new_width, new_height), Image.ANTIALIAS) #module 'PIL.Image' has no attribute 'ANTIALIAS'
-#     img.save(img_small_name)
-
-
-img_name = 'rgb888.png'
-IMG_HEIGHT = 256
-IMG_WIDTH = 256
-img = Image.open(img_name)
-
-# p = []
-# for r in range(32):
-#     for g in range(64):
-#         for b in range(32):
-#             # p.append([r*2/7,g/7,b*2/7])
-#             p.append([r*2/63,g/63,b*2/63])
-
-
 def get_new_val(old_val, nc):
     """
     Get the "closest" colour to old_val in the range [0,1] per channel divided
@@ -53,7 +15,7 @@ def get_new_val(old_val, nc):
 
         # https://stackoverflow.com/questions/12116830/numpy-slice-of-arbitrary-dimensions
 
-        # limit g to 62 to give g the same brightness range as r and b
+        # limit g to 62 to give g the same brightness range as r and b, this preserves pure white colors and simplifies calculation
         new_val[...,0] = np.round(old_val[...,0] * (31))*2 / 62
         new_val[...,1] = np.round(old_val[...,1] * (62))   / 62   
         new_val[...,2] = np.round(old_val[...,2] * (31))*2 / 62
@@ -70,10 +32,11 @@ def get_new_val(old_val, nc):
 # def get_new_val(old_val):
 #    idx = np.argmin(np.sum((old_val[None,:] - p)**2, axis=1))
 #    return p[idx]
+# TODO: fix this code and optimize it (searching full space is too costly with many colors, so only calculate subset of color space around rounded color value (-1,+0,+1 for all 3 colors? for a total of 27 colors to search?))
     
 def color_range_max(nc):
     if nc == 'rgb565':
-        # limit g to 62 to give g the same brightness range as r and b
+        # limit g to 62 to give g the same brightness range as r and b, this preserves pure white colors and simplifies calculation
         max = 248
     else:
         max = (nc-1)*(256/nc)
@@ -86,17 +49,18 @@ def fs_dither(img, nc):
 
     """
 
-    arr_raw = np.array(img, dtype=np.single)
     arr = np.array(img, dtype=np.single) / 255
     
+    IMG_HEIGHT = arr.shape[0]
+    IMG_WIDTH  = arr.shape[1]
 
     for ir in range(IMG_HEIGHT):
         for ic in range(IMG_WIDTH):
-            # NB need to copy here for RGB arrays otherwise err will be (0,0,0)!
-            old_val = arr[ir, ic].copy()
+            old_val = arr[ir, ic].copy() # copy to prevent overwriting old_val when writing to arr
             new_val = get_new_val(old_val, nc)
             arr[ir, ic] = new_val
             err = old_val - new_val
+
             # In this simple example, we will just ignore the border pixels.
             if ic < IMG_WIDTH - 1:
                 arr[ir, ic+1] += err * 7/16
@@ -107,38 +71,43 @@ def fs_dither(img, nc):
                 if ic < IMG_WIDTH - 1:
                     arr[ir+1, ic+1] += err / 16
 
-    # carr = np.array(arr/np.max(arr, axis=(0,1)) * 255, dtype=np.uint8) # if possible increase dynamic range, then map to 0-255 (may not be integer multuple)
-    # carr = np.array(arr * 255, dtype=np.uint8) # map to 0-255 (may not be integer multuple)
-                    
-    max = color_range_max(nc) # map to 0-max (is always integer multiple of nc)
-                            
-    carr = np.array(arr * max, dtype=np.uint8)
+    # carr = np.array(arr/np.max(arr, axis=(0,1)) * 255, dtype=np.uint8) # increase dynamic range if possible, then map to 0-255 (may not be integer multiple)
+    # carr = np.array(arr * 255, dtype=np.uint8) # map to 0-255 (may not be integer multiple of nc)                            
+    carr = np.array(arr * color_range_max(nc), dtype=np.uint8) # map to 0-max_of_nc (is always integer multiple of nc)   
 
-    # print(carr[0,0:20,:])
-    # print(carr[64,0:20,:])
     return Image.fromarray(carr)
 
 
 def palette_reduce(img, nc):
     """Simple palette reduction without dithering."""
     arr = np.array(img, dtype=np.single) / 255
+
+    # # increase dynamic range:
+    # arr = np.clip(arr * 2 - 0.10, 0, 1)
+
     arr = get_new_val(arr, nc)
 
-    max = max = color_range_max(nc)
+    max = color_range_max(nc)
 
-    carr = np.array(arr/np.max(arr) * max, dtype=np.uint8)
+    carr = np.array(arr * max, dtype=np.uint8)
     return Image.fromarray(carr)
 
-# rim = palette_reduce(img, 'rgb565')
-# rim.save('rimg-{}.png'.format('rgb565'))
 
-# for nc in (32,):
-# for nc in (2, 3, 4, 8, 16, 32):
-for nc in ('rgb565',): # (2, 3, 4, 8, 16):
-    print('nc =', nc)
-    dim = fs_dither(img, nc)
-    dim.save('img_{}_dither.png'.format(nc))
-    rim = palette_reduce(img, nc)
-    rim.save('img_{}_no_dither.png'.format(nc))
+if __name__ == "__main__":
+    img_name = 'rgb888.png'
+    #img_name = '1665_girl_with_a_pearl_earring_sm.jpg'
 
+    img = Image.open(img_name)
 
+    # # convert to greyscale
+    # img = img.convert('L')
+    # greyscale = True # g1
+
+    # for nc in (2,3,4):
+    for nc in (2,3,4,8,32,'rgb565',):
+    # for nc in ('rgb565',):
+        print(f'number of colors = {nc}')
+        dim = fs_dither(img, nc)
+        dim.save('img_g0_c{}_fs1.png'.format(nc))
+        rim = palette_reduce(img, nc)
+        rim.save('img_g0_c{}_fs0.png'.format(nc))
--- rgb565_validate.py
+++ rgb565_validate.py
@@ -1,10 +1,6 @@
 import numpy as np
 from PIL import Image
 
-img_name = 'rgb888.png'
-
-img_name = 'rgb565_no_dither.png'
-
 def validate(arr):
     # arr[0,0] = [8,4,8] #valid
     # arr[0,0] = [7,4,8] #not valid
@@ -14,8 +10,7 @@ def validate(arr):
     return np.all(arr&[2**(8-5)-1, 2**(8-6)-1, 2**(8-5)-1]==[0,0,0]) 
 
 
-img_names = ['rgb888.png','rgb565_no_dither.png','img_32_no_dither.png','img_32_dither.png','img_rgb565_no_dither.png','img_rgb565_dither.png',]
-
+img_names = ['rgb888.png','rgb565_no_dither.png','img_g0_c32_fs0.png','img_g0_c32_fs0.png','img_g0_crgb565_fs0.png','img_g0_crgb565_fs1.png',]
 for img_name in img_names:
     img = Image.open(img_name)
     arr = np.array(img)
